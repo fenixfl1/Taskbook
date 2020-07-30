ec787616bfe94a45eedf7a1aa914107fe3825a9f
Benjamin <43820285+fenixfl1@users.noreply.github.com>
2020-07-29 22:34:21 -0400

    Update production.py

--- config/production.py
+++ config/production.py
@@ -3,11 +3,7 @@ from .default import *
 
 SECURITY_EMAIL_SENDER = os.getenv('MAIL_USER')
 
-<<<<<<< HEAD
-ENV = os.getenv('APP_ENV')
-
 TESTING = False
 DEBUG = False
-=======
+
 ENV = os.getenv('FLASK_ENV')
->>>>>>> 8a3ff0439701a9d79847b1879434a12d9f2ec8fb
